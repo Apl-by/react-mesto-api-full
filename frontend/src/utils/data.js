b0722ea88854b3ca64c39f160b59d1d41cf3dbfc
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2022-11-22 16:17:40 +0300

    fix

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,5 +1,5 @@
 export const config = {
-  baseUrl: "https://api.mesto.apl-by.site",
+  baseUrl: "https://api.mesto.apl-by.ru",
   // baseUrl: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
